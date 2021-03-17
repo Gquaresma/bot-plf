3f2275b0cad72dcd3c969e56d4ba4b8ba2f132cf
Gabriel Quaresma <fulco02@gmail.com>
2021-03-17 19:43:37 -0300

    Array com os professores

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 
+const commands = require("./commads")
+const informations = require("./professores");
 const Discord = require("discord.js");
 const client = new Discord.Client({
     partials: ["MESSAGE"] //o bot terá acesso a coisas que aconteceram antes dele ser logado
 });
 
-const BOT_PREFIX = "!";
-const MOD_ME_COMMAND = "mod-me";
+
+
+const PREFIX = "!";
+// const MOD_ME_COMMAND = "mod-me";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
@@ -24,10 +28,20 @@ client.on('messageDelete', msg => {
 });
 
 client.on('message', msg => {
-    if(msg.content === "!plf-professor"){
-        msg.channel.send("E tem professor?");
+    if(!msg.content.startsWith(PREFIX)) return;
+
+    for(coman of commands){
+        for(information of informations){
+
+        }
+    }
+    if(msg.content === `${PREFIX}`){
+
+        msg.channel.send("Carvalho");
     }
     
+    
+
     if(msg.content === "Eu amo café"){
         msg.react("❤️");
     }
@@ -42,4 +56,4 @@ function modUser(member){
     member.roles.add("820329139747422209");
 }
 
-client.login(process.env.BOT_TOKEN);
+// client.login(process.env.BOT_TOKEN);
---
+++ professores.js
@@ -0,0 +1,120 @@
+module.exports = [
+    {
+        nome: "Fernando Carvalho",
+        materia: ["Org. Comp", "LP2", "PLF"],
+        horario: ["18h", "20h"],
+        contato: "Email - carvalho@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Alex Borges",
+        materia: ["Geometria"],
+        horario: ["18h", "20h"],
+        contato: "Email - alex@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Sônia",
+        materia: ["Sociedade da Informação", "Sistemas Multimídia", "TIEAD"],
+        horario: ["18h", "20h"],
+        contato: "Email - sonia@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Marjony",
+        materia: ["Comunicação", "Gerência de RH", "Marketing"],
+        horario: ["18h", "20h"],
+        contato: "Email - marjony@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Mirele",
+        materia: ["Cálculo 1", "M.Discreta", "Estágio Supervisionado"],
+        horario: ["18h", "20h"],
+        contato: "Email - mirele@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Jorge",
+        materia: ["LPA 1", "Sist. Op", "Sist. Distribuídos"],
+        horario: ["18h", "20h"],
+        contato: "Email - jorge@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Wylliams",
+        materia: ["Inglês", "Metodologia"],
+        horario: ["18h", "20h"],
+        contato: "Email - wylliams@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Pontual",
+        materia: ["Administração", "Seminários", "GTI"],
+        horario: ["18h", "20h"],
+        contato: "Email - pontual@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Jackson",
+        materia: ["LP1", ],
+        horario: ["18h", "20h"],
+        contato: "Email - jackson@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Moser",
+        materia: ["SI 1","SI2", "S. Cooperativos"],
+        horario: ["18h", "20h"],
+        contato: "Email - moser@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Cristovão",
+        materia: ["IPC","Probabilidade"],
+        horario: ["18h", "20h"],
+        contato: "Email - cristovao@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Paulemir",
+        materia: ["Int. Artificial","EDA", "Mineração de dados"],
+        horario: ["18h", "20h"],
+        contato: "Email - paulemir@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Elyda",
+        materia: ["Estágio Supervisionado", "Banco de dados 2"],
+        horario: ["18h", "20h"],
+        contato: "Email - elyda@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "P.Endo",
+        materia: ["Trabalho de Graduação", "Telecom"],
+        horario: ["18h", "20h"],
+        contato: "Email - endo@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Rômulo",
+        materia: ["LPA2", "Eng. Software", "Tópicos av. Eng. Sw"],
+        horario: ["18h", "20h"],
+        contato: "Email - romulo@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Erika",
+        materia: ["Cognição", "Redes 2", "C. Eletrônico"],
+        horario: ["18h", "20h"],
+        contato: "Email - erika@upe.br Celular - 9999999"
+    },
+
+    {
+        nome: "Miyachi",
+        materia: ["TIA"],
+        horario: ["18h", "20h"],
+        contato: "Email - miyachi@upe.br Celular - 9999999"
+    },
+]
